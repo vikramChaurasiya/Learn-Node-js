602ee3d420141fe4641ab2e307071936b027063d
vikramChaurasiya <bhartivikram633@gmail.com>
2025-03-19 15:57:11 +0530

    explore routing uses,get,post etc

--- DevTinder/src/app.js
+++ DevTinder/src/app.js
@@ -2,8 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req, res) => {
-    res.send("Namaste from the dashboard");
+app.use("/ab+c", (req,res)=>{
+    res.send("explore routing uses ab+c")
+})
+
+
+// this will only handle Get call to user
+app.get("/user", (req,res)=>{
+    res.send({Firstname:"vikram", lastname:"Chaurasiya"})
+})
+
+app.post("/user", (req,res)=>{
+    console.log("saved data to the databased");
+    res.send("Data successfully saved to the databased!")
+})
+
+app.delete("/user", (req, res)=>{
+    res.send("deleted successfully");
+})
+
+
+// this will match all the HTTP method API calls to /test
+app.use("/test",(req, res) => {
+    res.send("test hello");
+})
+
+app.use("/hello",(req, res)=>{
+    res.send("hello hello i am from hello")
 })
 
 app.listen(3000, ()=>{
