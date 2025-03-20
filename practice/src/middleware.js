fa30a9d99b57603e3d9d6a2c67de3eb5bcf4c6a1
vikramChaurasiya <bhartivikram633@gmail.com>
2025-03-20 23:25:30 +0530

    middleware concept

---
+++ practice/src/middleware.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const app = express();
+
+app.use
+    ("/user",(req, res, next) =>{
+    console.log("runn file");
+    next();
+    },
+    (req,res,next)=>{
+        console.log("again");
+        next();
+    },
+    (req,res)=>{
+        res.send("user data is collect2")
+    }
+)
+
+app.listen(3002, ()=>{
+    console.log("server is successfully is run port 3002...");
+    
+})
